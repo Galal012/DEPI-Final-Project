3f895804e779a53aa2396275b87f727ba4e04790
Galal <8ballpool01279@gmail.com>
2025-11-21 04:42:16 +0200

    Fixed some errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,9 @@ import { Routes } from "react-router";
 import { Route } from "react-router";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
-import { LanguageProvider } from "./contexts/LanguageContext";
 import TopBar from "./Components/TopBar";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer/Footer";
-import LanguageDebug from "./Components/LanguageDebug";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
@@ -17,7 +15,6 @@ import Blog from "./Pages/Blog";
 import BlogSingle from "./Pages/BlogSingle";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Pricing from "./Pages/Pricing";
 import MakeAppointment from "./Pages/MakeAppointment";
 import Dashboard from "./Pages/Dashboard";
 import { Toaster } from "react-hot-toast";
@@ -40,33 +37,29 @@ function App() {
   }, [i18n.language]);
 
   return (
-    <LanguageProvider>
-      <div className={`min-h-screen ${isRTL ? "rtl" : "ltr"}`}>
-        {/* {<LanguageDebug />} */}
-        <Toaster
-          position="top-center" // You can change this to 'top-center', 'bottom-right', etc.
-          reverseOrder={false}
-        />
-        <TopBar />
-        <Navbar />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogSingle />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/appointment" element={<MakeAppointment />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-        <Footer />
-      </div>
-    </LanguageProvider>
+    <div className={`min-h-screen ${isRTL ? "rtl" : "ltr"}`}>
+      <Toaster
+        position="top-center" // You can change this to 'top-center', 'bottom-right', etc.
+        reverseOrder={false}
+      />
+      <TopBar />
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogSingle />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/appointment" element={<MakeAppointment />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
